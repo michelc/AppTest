3f9270ced8459e93c431976857dfb6efa61a4071
Michel <michelc@gmail.com>
2019-09-10 19:09:42 +0200

    Commentaires sur l'organisation de Index.js.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const path = require("path");
 
+// Création du serveur Express
 const app = express();
 
+// Configuration du serveur
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Démarrage du serveur
 app.listen(3000, () => {
-  console.log("Serveur démarré (http://localhost:3000/) !");
+    console.log("Serveur démarré (http://localhost:3000/) !");
 });
 
+// GET /
 app.get("/", (req, res) => {
   // res.send("Bonjour le monde...");
   res.render("index");
 });
 
+// GET /about
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
+// GET /data
 app.get("/data", (req, res) => {
   const test = {
     titre: "Test",
