5adc8870283af3bc3bd088f9b762c185cd859511
Michel <michelc@gmail.com>
2019-09-10 19:05:32 +0200

    Utilisation de la vue Index.ejs dans Express.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
   console.log("Serveur démarré (http://localhost:3000/) !");
 });
 
 app.get("/", (req, res) => {
-  res.send("Bonjour le monde...");
+  // res.send("Bonjour le monde...");
+  res.render("index");
 });
